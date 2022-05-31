19c3dfba91919a37846b75cb91f2431cbbc90fb1
Vishv Gajjar <60516442+vishv-gajjar@users.noreply.github.com>
2022-05-31 19:22:08 +0530

    Add files via upload

---
+++ TATVASOFT book store/src/components/MainNavigation.tsx
@@ -0,0 +1,20 @@
+import React, { lazy } from "react";
+import { Route, Switch } from "react-router-dom";
+import { RoutePaths } from "../utils/enum";
+
+//component lazy loading
+const Home = lazy(() => import("../pages/login/index"));
+const Login = lazy(() => import("../pages/login/index"));
+const Register = lazy(() => import("../pages/register"));
+
+const AppRoutes: React.FC = () => {
+  return (
+    <Switch>
+      <Route exact path={RoutePaths.Home} component={Home} />
+      <Route exact path={RoutePaths.Login} component={Login} />
+      <Route exact path={RoutePaths.Register} component={Register} />
+    </Switch>
+  );
+};
+
+export default AppRoutes;
